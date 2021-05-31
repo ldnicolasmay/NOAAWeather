d82bc8669a7fe23da8de7cfb1338490ec901e776
ldnicolasmay <ldnicolasmay@gmail.com>
2021-05-31 14:31:02 -0400

    Comment out logging

--- lib/noaa_weather/noaa_weather.ex
+++ lib/noaa_weather/noaa_weather.ex
@@ -1,11 +1,11 @@
 defmodule NOAAWeather do
 
-  require Logger
+  # require Logger
 
   @noaa_url Application.get_env(:noaa_weather, :noaa_weather_url)
 
   def fetch(icao_code) do 
-    Logger.info("Fetching weather at #{icao_code}")
+    # Logger.info("Fetching weather at #{icao_code}")
 
     weather_url(icao_code)
     |> HTTPoison.get()
@@ -15,8 +15,8 @@ defmodule NOAAWeather do
   defp weather_url(icao_code), do: "#{@noaa_url}?stid=#{icao_code}"
 
   defp handle_response({_, %{status_code: status_code, body: body}}) do 
-    Logger.info("Got reponse: status_code=#{status_code}")
-    Logger.debug(fn -> IO.inspect(body) end)
+    # Logger.info("Got reponse: status_code=#{status_code}")
+    # Logger.debug(fn -> IO.inspect(body) end)
 
     {
       status_code |> check_for_error(),
